197aa45c837e917de10915b61eed6151bc7070d4
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2016-01-27 08:07:51 +0100

    Use [4][4] matrices, convert it to [16] just before passing it to OpenGL

--- src/gtkscope.d
+++ src/gtkscope.d
@@ -101,15 +101,18 @@ private:
   bool render(GLContext, GLArea a)
   {
     makeCurrent();
+    // glEnable(GL_DEPTH_TEST);
 
-    const c = 0.5;
+    const c = 0.65;
     glClearColor(c, c, c, 1);
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     {
       const rotationX = makeRotationMatrixY(m_rotationX + m_dragRotationX);
       const rotationY = makeRotationMatrixX(m_rotationY + m_dragRotationY);
-      auto mvp = scaleMatrix(exp(m_zoom * 0.1));
+      auto mvp = identityMatrix();
+      // mvp = multMatrix(mvp, perspectiveMatrix(1.6, 1.0, 0.01, 100));
+      mvp = multMatrix(mvp, scaleMatrix(exp(m_zoom * 0.1)));
 
       mvp = multMatrix(mvp, rotationX);
       mvp = multMatrix(mvp, rotationY);
@@ -117,7 +120,14 @@ private:
       glUseProgram(m_Program);
 
       // update the "mvp" matrix we use in the shader
-      glUniformMatrix4fv(m_Mvp, 1, GL_FALSE, mvp.ptr);
+      {
+        GLfloat[16] mat;
+
+        foreach(idx, ref val; mat)
+          val = mvp[idx / 4][idx % 4];
+
+        glUniformMatrix4fv(m_Mvp, 1, GL_FALSE, mat.ptr);
+      }
 
       glBindBuffer(GL_ARRAY_BUFFER, m_Vao);
       glEnableVertexAttribArray(0);
@@ -228,72 +238,72 @@ unittest
   assert(identityMatrix() == multMatrix(identityMatrix(), identityMatrix()));
 }
 
-float[16] makeRotationMatrixX(float theta)
+float[4][4] makeRotationMatrixX(float theta)
 {
   const cosTheta = cast(float) cos(theta);
   const sinTheta = cast(float) sin(theta);
 
   return [
-    1.0f, 0.0f, 0.0f, 0.0f,
-    0.0f, cosTheta, -sinTheta, 0.0f,
-    0.0f, sinTheta, cosTheta, 0.0f,
-    0.0f, 0.0f, 0.0f, 1.0f,
+    [1.0f, 0.0f, 0.0f, 0.0f],
+    [0.0f, cosTheta, -sinTheta, 0.0f],
+    [0.0f, sinTheta, cosTheta, 0.0f],
+    [0.0f, 0.0f, 0.0f, 1.0f],
   ];
 }
 
-float[16] makeRotationMatrixY(float theta)
+float[4][4] makeRotationMatrixY(float theta)
 {
   const cosTheta = cast(float) cos(theta);
   const sinTheta = cast(float) sin(theta);
 
   return [
-    cosTheta, 0.0f, sinTheta, 0.0f,
-    0.0f, 1.0f, 0.0f, 0.0f,
-    -sinTheta, 0.0f, cosTheta, 0.0f,
-    0.0f, 0.0f, 0.0f, 1.0f,
+    [cosTheta, 0.0f, sinTheta, 0.0f],
+    [0.0f, 1.0f, 0.0f, 0.0f],
+    [-sinTheta, 0.0f, cosTheta, 0.0f],
+    [0.0f, 0.0f, 0.0f, 1.0f],
   ];
 }
 
-float[16] makeRotationMatrixZ(float theta)
+float[4][4] makeRotationMatrixZ(float theta)
 {
   const cosTheta = cast(float) cos(theta);
   const sinTheta = cast(float) sin(theta);
 
   return [
-    cosTheta, -sinTheta, 0.0f, 0.0f,
-    sinTheta, cosTheta, 0.0f, 0.0f,
-    0.0f, 0.0f, 1.0f, 0.0f,
-    0.0f, 0.0f, 0.0f, 1.0f,
+    [cosTheta, -sinTheta, 0.0f, 0.0f],
+    [sinTheta, cosTheta, 0.0f, 0.0f],
+    [0.0f, 0.0f, 1.0f, 0.0f],
+    [0.0f, 0.0f, 0.0f, 1.0f],
   ];
 }
 
-float[16] identityMatrix()
+float[4][4] identityMatrix()
 {
-  float r[4 * 4];
+  float r[4][4];
 
   for(int x = 0; x < 4; ++x)
     for(int y = 0; y < 4; ++y)
-      r[x + y * 4] = x == y ? 1 : 0;
+      r[x][y] = x == y ? 1 : 0;
 
   return r;
 }
 
-float[16] scaleMatrix(float s)
+float[4][4] scaleMatrix(float s)
 {
-  float r[4 * 4];
+  float r[4][4];
 
   for(int x = 0; x < 4; ++x)
     for(int y = 0; y < 4; ++y)
-      r[x + y * 4] = x == y ? s : 0;
+      r[x][y] = x == y ? s : 0;
 
-  r[15] = 1;
+  r[3][3] = 1;
 
   return r;
 }
 
-float[16] multMatrix(float[16] a, float[16] b)
+float[4][4] multMatrix(float[4][4] a, float[4][4] b)
 {
-  float[16] r;
+  float[4][4] r;
 
   for(int row = 0; row < 4; ++row)
   {
@@ -302,12 +312,33 @@ float[16] multMatrix(float[16] a, float[16] b)
       float val = 0;
 
       for(int k = 0; k < 4; ++k)
-        val += a[k + row * 4] * b[col + k * 4];
+        val += a[k][row] * b[col][k];
 
-      r[col + row * 4] = val;
+      r[col][row] = val;
     }
   }
 
+  return r;
+}
+
+float[4][4] perspectiveMatrix(float fovy, float aspect, float zNear, float zFar)
+{
+  assert(aspect != 0);
+  assert(zFar != zNear);
+
+  const tanHalfFovy = tan(fovy / 2.0f);
+
+  float[4][4] r;
+
+  for(int x = 0; x < 4; ++x)
+    for(int y = 0; y < 4; ++y)
+      r[x][y] = 0;
+
+  r[0][0] = 1.0f / (aspect * tanHalfFovy);
+  r[1][1] = 1.0f / (tanHalfFovy);
+  r[2][2] = -(zFar + zNear) / (zFar - zNear);
+  r[2][3] = -1.0f;
+  r[3][2] = -(2.0f * zFar * zNear) / (zFar - zNear);
   return r;
 }
 
